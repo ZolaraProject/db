b028fd8b462281ab99f70f3110f94dea204dcbe5
Kitae SEO <kitae.seo@epitech.eu>
2024-06-10 14:35:47 +0000

    fixed users table column order

--- init.sql
+++ init.sql
@@ -23,8 +23,8 @@ CREATE TYPE user_role AS ENUM ('admin', 'user');
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     role user_role DEFAULT 'user'
 );
 
