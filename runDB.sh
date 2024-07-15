95ca1c457cc5588f2497801d1aa82fe59feb73fc
Kitae SEO <kitae.seo@epitech.eu>
2024-07-15 13:08:36 +0000

    updated runDB script not to run DB container

--- runDB.sh
+++ runDB.sh
@@ -1,2 +1,2 @@
 docker build -t postgres .
-docker run -d --restart unless-stopped --name postgres -p 127.0.0.1:5432:5432 postgres
+# docker run -d --restart unless-stopped --name postgres -p 127.0.0.1:5432:5432 postgres
