001fa0ea144db6ba5549c304db67ecef3036fea1
Kitae SEO <kitae.seo@epitech.eu>
2024-09-12 13:28:00 +0000

    externalized db password

--- runDB.sh
+++ runDB.sh
@@ -1,2 +1,5 @@
-docker build -t postgres .
+docker secret create postgres_password ./secrets/postgres-password
+docker build --secret id=postgres_password,src=secrets/postgres-password -t postgres .
+
+# docker build --build-arg POSTGRES_PASSWORD=$POSTGRES_PASSWORD -t postgres .
 # docker run -d --restart unless-stopped --name postgres -p 127.0.0.1:5432:5432 postgres
