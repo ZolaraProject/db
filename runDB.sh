1d30053e7a7cdede6e9b79b39c6d3e51acd7bca9
Kitae SEO <kitae.seo@epitech.eu>
2024-10-24 08:16:54 +0000

    fixed typo

--- runDB.sh
+++ runDB.sh
@@ -1,5 +1,5 @@
 docker secret create postgres_password ./secrets/postgres-password
-docker build --secret id=postgres_password,src=secrets/postgres-password -t postgres:ZOLATA_TAG .
+docker build --secret id=postgres_password,src=secrets/postgres-password -t postgres:ZOLARA_TAG .
 
 # docker build --build-arg POSTGRES_PASSWORD=$POSTGRES_PASSWORD -t postgres .
 # docker run -d --restart unless-stopped --name postgres -p 127.0.0.1:5432:5432 postgres
