2ceb8d9ff3a607238bf7ea26869bf9320ba9a1b3
Kitae SEO <kitae.seo@epitech.eu>
2025-03-06 15:34:14 +0000

    updated liquibase

--- liquibase.sh
+++ liquibase.sh
@@ -40,11 +40,10 @@ echo "Waiting for the reference schema in VSCode... "
 echo "-- Paste here reference schema then close the file" > ref.sql && code ref.sql --wait
 
 echo "Generating reference dump..."
-sudo docker run --name postgresqldiff -e POSTGRES_PASSWORD=postgres -p 5443:5432 -d postgres:16.3 > /dev/null
+sudo docker run --name postgresqldiff -e POSTGRES_PASSWORD=zolara_changeme -p 5443:5432 -d postgres:16.3 > /dev/null
 
 echo "Waiting for PostgreSQL to be ready..."
-export PGPASSWORD="postgres"
-until sudo docker exec -it postgresqldiff psql -U postgres -c '\q' 2> /dev/null; do sleep 1; done
+until PGPASSWORD=zolara_changeme sudo docker exec -it postgresqldiff psql -U postgres -c '\q' 2> /dev/null; do sleep 1; done
 echo "PostgreSQL is ready, launching Liquibase"
 
 sudo docker exec -i postgresqldiff psql -U postgres -c "CREATE DATABASE zolara;"
@@ -54,7 +53,7 @@ sudo docker exec -i postgresqldiff psql -U postgres -d zolara < ref.sql
 sudo docker exec -i postgresqldiff psql -U postgres -d zolara -c '\dt'
 
 # Generate the diff
-sudo docker run -v ${PWD}:/liquibase/changelog -v ${PWD}/out:/out --network host liquibase/liquibase --defaultsFile=/liquibase/changelog/$chosen_property --referenceUrl="jdbc:postgresql://127.0.0.1:5443/zolara" --referenceUsername=postgres  --referencePassword=postgres --changeLogFile=/out/diff.sql diffChangeLog
+sudo docker run -v ${PWD}:/liquibase/changelog -v ${PWD}/out:/out --network host liquibase/liquibase --defaultsFile=/liquibase/changelog/$chosen_property --referenceUrl="jdbc:postgresql://127.0.0.1:5443/zolara" --referenceUsername=postgres  --referencePassword=zolara_changeme --changeLogFile=/out/diff.sql diffChangeLog
 
 # Check if diff.sql is generated
 if [ -f out/diff.sql ]; then
